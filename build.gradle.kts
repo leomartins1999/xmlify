7367da63d8cc5e1769bc19cbee43bae796ec047d
leomartins1999 <36698491+leomartins1999@users.noreply.github.com>
2022-04-16 19:34:51 +0100

    Add KDocs to readme (#19)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
 
 group = "com.github.leomartins1999"
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -32,9 +33,13 @@ tasks.test {
 tasks.withType<DokkaTask>().configureEach {
     dokkaSourceSets {
         named("main") {
-            moduleName.set("XMLify")
-
+            moduleName.set("xmlify")
             documentedVisibilities.set(setOf(org.jetbrains.dokka.DokkaConfiguration.Visibility.PUBLIC))
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/leomartins1999/xmlify/tree/main/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
         }
     }
 }
