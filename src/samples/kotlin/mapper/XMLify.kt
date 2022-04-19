f8dc1c04d3b42c62a69b7ed6da6eabbea5b6820b
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2022-04-19 22:53:53 +0100

    Add support for enums in mapper module

--- src/samples/kotlin/mapper/XMLify.kt
+++ src/samples/kotlin/mapper/XMLify.kt
@@ -2,20 +2,33 @@ package mapper
 
 import com.github.leomartins1999.xmlify.mapper.xmlify
 
+enum class Numbers { ONE, TWO, THREE }
+
 fun xmlifyExample() {
     /**
-     * Maps an object to an Element (Tree Element in this case)
+     * Maps an object to a Tree Element
      */
     data class SoccerPlayer(
         val name: String,
         val number: Int,
         val retired: Boolean
     )
+
     val player = SoccerPlayer("Bernardo Silva", 20, false)
     val objectElement = xmlify { player }
 
     /**
-     * Maps a value to an Element (Leaf Element in this case)
+     * Maps a value to a Leaf Element
      */
     val valueElement = xmlify { "Flower" }
+
+    /**
+     * Maps a collection to a TreeElement
+     */
+    val collectionElement = xmlify { listOf(1, 2, 3, 4, 5) }
+
+    /**
+     * Maps an enum to a Leaf Element
+     */
+    val enumElement = xmlify { Numbers.ONE }
 }
---
+++ src/test/kotlin/com/github/leomartins1999/xmlify/mapper/EnumMapperTests.kt
@@ -0,0 +1,22 @@
+package com.github.leomartins1999.xmlify.mapper
+
+import com.github.leomartins1999.xmlify.model.LeafElement
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class EnumMapperTests {
+
+    enum class Direction {
+        NORTH, SOUTH, WEST, EAST
+    }
+
+    @Test
+    fun `maps an enum by it's name`() {
+        val dir = Direction.NORTH
+
+        val element = xmlify { dir }
+
+        assertEquals(LeafElement("Direction", "NORTH"), element)
+    }
+
+}
