59e38ef3d23a5e67c05100315f1fdbb994515810
leomartins1999 <36698491+leomartins1999@users.noreply.github.com>
2022-04-19 22:40:42 +0100

    Add support for collections (#26)

---
+++ src/main/kotlin/com/github/leomartins1999/xmlify/mapper/strategies/CollectionMappingStrategy.kt
@@ -0,0 +1,15 @@
+package com.github.leomartins1999.xmlify.mapper.strategies
+
+import com.github.leomartins1999.xmlify.mapper.xmlify
+import com.github.leomartins1999.xmlify.model.Element
+import com.github.leomartins1999.xmlify.model.element
+
+object CollectionMappingStrategy : MappingStrategy {
+    override fun toElement(instance: Any): Element {
+        instance as Collection<*>
+
+        val children = instance.map { xmlify { it } }
+
+        return element("collection", children)
+    }
+}
